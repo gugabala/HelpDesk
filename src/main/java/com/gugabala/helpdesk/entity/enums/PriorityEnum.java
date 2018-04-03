68eaf51ec5513e3d024673b82aa11e41049c30ef
gugabala <gugabala@gmail.com>
2018-04-03 13:39:52 -0300

    Criando Entidades e enums

---
+++ src/main/java/com/gugabala/helpdesk/entity/enums/PriorityEnum.java
@@ -0,0 +1,5 @@
+package com.gugabala.helpdesk.entity.enums;
+
+public enum PriorityEnum {
+	High, Normal, Low
+}
---
+++ src/main/java/com/gugabala/helpdesk/entity/enums/ProfileEnum.java
@@ -0,0 +1,5 @@
+package com.gugabala.helpdesk.entity.enums;
+
+public enum ProfileEnum {
+	ROLE_ADMIN, ROLE_CUSTOMER, ROLE_TECHNICIAN
+}
